54ce76e00bcfa09b50eff00ab915752843b2ba69
missitunu <omolekeitunu44@gmail.com>
2024-06-01 07:21:03 +0100

    feat:logics and screen (#3)
    Co-authored-by: haastrup elijah <41439020+haastrupea@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Cloud1 from './Cloud1';
+import logo from './logo.svg';
 import './App.css';
 import SearchScreen from './components/SearchScreen';
-
 function App() {
   return (
     <div className="App">
-      <SearchScreen />
+      <Cloud1 />
+    <SearchScreen />
     </div>
   );
 }
